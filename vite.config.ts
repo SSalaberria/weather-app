af35b6797a2eed5529539e3f5f13b1d959e1cca2
Sebastian Salaberria <s_salaberria@hotmail.com>
2023-02-22 20:54:19 -0300

    more PWA stuff

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,36 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Weathery - for all your weather needs",
+        short_name: "Weathery",
+        description: "Weathery is another weather app you didn't need nor did you ask for.",
+        theme_color: "#1F1F42",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
